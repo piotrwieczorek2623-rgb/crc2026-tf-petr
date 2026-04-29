b2981b3ebf5e64b8d5b0b7f57b742e7b8a47955b
peter multimeter <piotr.wieczorek2623@gmail.com>
2026-04-29 17:27:31 +0200

    upload files

---
+++ data.tf
@@ -0,0 +1,4 @@
+data "azurerm_resource_group" "default" {
+  name = "rg-crc2026-student-209-lab"
+}
+
---
+++ providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~> 1.10"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.70.0"
+    }
+  }
+
+   backend "azurerm" {
+    resource_group_name  = "rg-crc2026-student-209-lab"
+    storage_account_name = "pwieczoreksatf"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  resource_provider_registrations = "none"
+}
+
